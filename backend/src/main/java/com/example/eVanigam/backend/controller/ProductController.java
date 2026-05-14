951af68b0f4041c3b811398ba1b97603c4830d95
Mathesh <matheshmani1707@gmail.com>
2026-05-14 22:35:43 +0530

    Implemented basic CRUD operations

--- backend/src/main/java/com/example/eVanigam/backend/controller/ProductController.java
+++ backend/src/main/java/com/example/eVanigam/backend/controller/ProductController.java
@@ -1,6 +1,13 @@
 package com.example.eVanigam.backend.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.eVanigam.backend.model.Product;
@@ -13,8 +20,24 @@ public class ProductController {
     public ProductController (ProductService service){
         this.service=service;
     }
-    @GetMapping
+    @PostMapping
     public Product addProduct(Product product) {
         return service.addProduct(product);
+    }
+    @GetMapping
+    public List<Product> getProducts() {
+        return service.getProducts();
+    }
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return service.getProductById(id);
+    }
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return service.updateProduct(id, product);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        service.deleteProduct(id);
     }
 }
--- backend/src/main/java/com/example/eVanigam/backend/service/ProductService.java
+++ backend/src/main/java/com/example/eVanigam/backend/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.example.eVanigam.backend.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.example.eVanigam.backend.model.Product;
@@ -17,4 +19,31 @@ public class ProductService {
     public Product addProduct(Product product) {
         return repo.save(product);
     }
+
+    public List<Product> getProducts() {
+        return repo.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+    }
+    
+    public void deleteProduct(Long id) {
+        repo.deleteById(id);
+    }
+
+    public Product updateProduct(Long id, Product product) {
+        Product existing = repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+        existing.setCategory(product.getCategory());
+        existing.setDescription(product.getDescription());
+        existing.setImageUrl(product.getImageUrl());
+        existing.setName(product.getName());
+        existing.setStock(product.getStock());
+        existing.setprice(product.getprice());
+
+        return repo.save(existing);
+    }
+
 }
